52751adb86320226341d9c3417ee8cd54969324c
thepantalion <pantaranieri@gmail.com>
2022-04-04 16:25:14 +0200

    initial commit

---
+++ src/main/java/it/polimi/tiw/tiwproject/HelloApplication.java
@@ -0,0 +1,9 @@
+package it.polimi.tiw.tiwproject;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
