f8303216ac18eb2818d30d56ec356906ab96de6f
thepantalion <pantaranieri@gmail.com>
2022-04-16 17:02:09 +0200

    db dump was not set properly

--- db_tiw_project_dump.sql
+++ db_tiw_project_dump.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `db_tiw_project` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `db_tiw_project`;
 -- MySQL dump 10.13  Distrib 8.0.28, for macos11 (x86_64)
 --
 -- Host: 127.0.0.1    Database: db_tiw_project
@@ -109,4 +111,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-04-16 16:43:02
+-- Dump completed on 2022-04-16 17:00:16
