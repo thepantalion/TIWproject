2db8ddd260c758cc8937eeb7d8c571a7b3b073f3
thepantalion <pantaranieri@gmail.com>
2022-05-14 13:04:30 +0200

    chiara takeover of meeting creation

--- src/main/java/it/polimi/tiw/tiwproject/controllers/CreateMeeting.java
+++ src/main/java/it/polimi/tiw/tiwproject/controllers/CreateMeeting.java
@@ -1,17 +1,52 @@
 package it.polimi.tiw.tiwproject.controllers;
 
+import it.polimi.tiw.tiwproject.utilities.ConnectionHandler;
 import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templatemode.TemplateMode;
+import org.thymeleaf.templateresolver.ServletContextTemplateResolver;
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 import java.sql.Connection;
 
 @WebServlet("/CreateMeeting")
 public class CreateMeeting extends HttpServlet {
-    private final Connection connection = null;
+    private Connection connection;
     private TemplateEngine templateEngine;
 
     public CreateMeeting() {
         super();
+    }
+
+    public void init() throws ServletException {
+        connection = ConnectionHandler.getConnection(getServletContext());
+        ServletContext servletContext = getServletContext();
+        ServletContextTemplateResolver templateResolver = new ServletContextTemplateResolver(servletContext);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        this.templateEngine = new TemplateEngine();
+        this.templateEngine.setTemplateResolver(templateResolver);
+        templateResolver.setSuffix(".html");
+        connection = ConnectionHandler.getConnection(getServletContext());
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        HttpSession session = request.getSession();
+        if (session.isNew() || session.getAttribute("user") == null) {
+            String loginpath = getServletContext().getContextPath() + "/index.html";
+            response.sendRedirect(loginpath);
+            return;
+        }
+
+
     }
 }
